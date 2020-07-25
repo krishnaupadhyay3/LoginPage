19de1f1ef9076949c0ba71155a2eefa23862cd79
krishna <krishna.ramg003@gmail.com>
2020-07-25 22:04:07 +0530

    added js file , register button logic

---
+++ static/js/main.js
@@ -0,0 +1,12 @@
+
+function ShowSignup() {
+  $(".login-btn").hide();
+  $(".email-input").show();
+    $(".register-btn").show();
+}
+
+ function ShowLogin() {
+$(".email-input").hide();
+  $(".register-btn").hide();
+  $(".login-btn").show();
+}
